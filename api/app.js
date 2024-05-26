2d155e22d7e6897265b88c596f59321eb7f36c9c
erajneeshverma <rajneeshverma94306@gmail.com>
2024-05-26 18:44:59 +0530

    backend deployedment process

---
+++ api/app.js
@@ -0,0 +1,59 @@
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import path from 'path';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from 'morgan';
+const app = new express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(morgan("dev")); //HTTP request logger middleware for node.js 
+
+
+
+const __dirname = path.resolve();
+
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
+
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+
+export default app;
